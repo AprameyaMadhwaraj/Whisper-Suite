7c632bb845134053cad3f1cc663f91b2efe3da21
Apram <113331508+AprameyaMadhwaraj@users.noreply.github.com>
2024-05-02 15:37:43 +0530

    Update live_whisperX_client.py

--- WhisperX Approach/live_whisperX_client.py
+++ WhisperX Approach/live_whisperX_client.py
@@ -1,77 +1,57 @@
 import asyncio
+import time
+
 import websockets
 import pyaudio
 import base64
-import os
-import datetime
-#import shutil
-import wave
 
 # Audio parameters
 CHUNK = 4096
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
-RECORD_SECONDS = 6
+RECORD_SECONDS = 8
 
-local_directory_name = "D:/Personal/Aikenist/Client_audio"
+async def send_audio_data(websocket):
+    try:
+        audio = pyaudio.PyAudio()
 
-async def record_and_transmit():
+        stream = audio.open(format=FORMAT,
+                            channels=1,
+                            rate=RATE,
+                            input=True,
+                            frames_per_buffer=CHUNK)
+
+        print("\nListening... Press Ctrl+C to stop.")
+
+        while True:
+            frames = []
+            print("Recording...")
+            for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+                data = stream.read(CHUNK, exception_on_overflow = False)
+                frames.append(data)
+
+            # Convert binary audio data to base64
+            audio_data = base64.b64encode(b''.join(frames)).decode('utf-8')
+
+            # Send base64 encoded audio data to the server
+            await websocket.send(audio_data)
+
+            response = await websocket.recv()
+            print("Received response:", response, flush=True)
+
+    except KeyboardInterrupt:
+        print("\nSending audio data stopped.")
+    finally:
+        # Clean up
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+async def main():
     async with websockets.connect("ws://localhost:8765") as websocket:
-    #async with websockets.connect("ws://localhost:8765", ping_interval=None) as websocket:
-        try:
-            audio = pyaudio.PyAudio()
-
-            stream = audio.open(format=FORMAT,
-                                channels=CHANNELS,
-                                rate=RATE,
-                                input=True,
-                                frames_per_buffer=CHUNK)
-
-            print("\nListening... Press Ctrl+C to stop.")
-
-            while True:
-                frames = []
-
-                for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-                    data = stream.read(CHUNK)
-                    frames.append(data)
-
-                # For Saving Audio
-                current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S") 
-                filename = os.path.join(local_directory_name, f"audio_{current_time}.wav")
-
-                # Save audio frames to a new file
-                with wave.open(filename, 'wb') as wf:
-                    wf.setnchannels(CHANNELS)
-                    wf.setsampwidth(audio.get_sample_size(FORMAT))
-                    wf.setframerate(RATE)
-                    wf.writeframes(b''.join(frames))
-
-                
-                # Convert binary audio data to base64
-                audio_data = base64.b64encode(b''.join(frames)).decode('utf-8')
-
-                # Send base64 encoded audio data to the server
-                await websocket.send(audio_data)
-
-                # Receive transcription from the server
-                response = await websocket.recv()
-                print(response, end='', flush=True)  # Flush the output buffer after each print
-
-        except KeyboardInterrupt:
-            print("\n \t RECORDING STOPPED.")
-        finally:
-            # Clean up
-            stream.stop_stream()
-            stream.close()
-            audio.terminate()
-
+        # Start sending audio data and receiving responses concurrently
+        await asyncio.gather(send_audio_data(websocket))
 
 if __name__ == "__main__":
-    try:
-        asyncio.get_event_loop().run_until_complete(record_and_transmit())
-    except KeyboardInterrupt:
-        print("\n \t CLIENT STOPPED.")
-
-    
+    asyncio.run(main())
